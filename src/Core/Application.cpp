d13bc732b28f5c8ee96e84259af1680b285ebd6d
Sajko <kamil.sajdak@op.pl>
2020-05-15 13:27:14 +0200

    Application class

---
+++ src/Core/Application.cpp
@@ -0,0 +1,6 @@
+#include "Application.hpp"
+
+void core::Application::run()
+{
+	LOG_INFO("Running...");
+}
---
+++ src/Core/Application.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <memory>
+
+#include "Renderer.hpp"
+
+namespace core {
+
+class Application final {
+public:
+
+	static Application& getInstance() {
+		static Application app;
+		return app;
+	}
+
+	float getTime() const { return _deltaTime; }
+
+	void run();
+
+	~Application() = default;
+
+	Application(Application&) = delete;
+	Application(Application&&) = delete;
+	Application& operator=(const Application&) = delete;
+	Application& operator=(Application&&) = delete;
+private:
+	Application() = default;
+	
+	float _deltaTime;
+};
+
+}
