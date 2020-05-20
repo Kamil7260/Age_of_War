afa1cc1a2e8e45947db3e276dfea7ae5097a040f
Sajko <kamil.sajdak@op.pl>
2020-05-21 00:25:34 +0200

    Enemy class

---
+++ src/GameClass/Enemy.cpp
@@ -0,0 +1,64 @@
+#include "Enemy.hpp"
+#include "../Core/ResourceManager.hpp"
+#include "../Core/Application.hpp"
+#include "../GameClass/Melee.hpp"
+
+Enemy::Enemy()
+	:_timer(0.f)
+{
+	_team = base::team::enemy;
+	_sprite.setTexture(*core::ResourceManager<sf::Texture>::getInstance().get("Assets/base/1.png"));
+	setScale(sf::Vector2f(-1.f, 1.f));
+	_sprite.setOrigin(275.f, 170.f);
+	_myColider = { 111.f,111.f,111.f,111.f };
+}
+
+Enemy::Enemy(Enemy&& source) noexcept
+	:_timer(source._timer), _sprite(source._sprite)
+{
+}
+
+Enemy& Enemy::operator=(Enemy&& source) noexcept
+{
+	_timer = source._timer;
+	_sprite = source._sprite;
+	return *this;
+}
+
+void Enemy::setPosition(const sf::Vector2f& pos)
+{
+	_position = pos;
+	_sprite.setPosition(pos);
+}
+
+void Enemy::setScale(const sf::Vector2f& sca)
+{
+	_scale = sca;
+	_sprite.setScale(sca);
+}
+
+void Enemy::move(const sf::Vector2f& delta)
+{
+	_sprite.move(delta);
+	_position += delta;
+}
+
+void Enemy::onUpdate()
+{
+	_timer += core::Application::getInstance().getTime();
+
+	if (_timer >= 5)
+	{
+		std::unique_ptr<Melee> man = std::make_unique<Melee>();
+		man->setAnimatorName("caveman");
+		man->setPosition(sf::Vector2f(_position.x - 100, 850));
+		core::Renderer::getInstance().addEnemyObject(std::move(man));
+		_timer = -1000;
+	}
+
+}
+
+void Enemy::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.draw(_sprite, states);
+}
---
+++ src/GameClass/Enemy.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "../Base/Stronghold.hpp"
+
+class Enemy : public base::Stronghold {
+public:
+	Enemy();
+	Enemy(const Enemy&) = delete;
+	Enemy(Enemy&&) noexcept;
+	Enemy& operator=(const Enemy&) = delete;
+	Enemy& operator=(Enemy&&) noexcept;
+
+	virtual void setPosition(const sf::Vector2f& pos) override;
+	virtual void setScale(const sf::Vector2f& sca) override;
+
+	virtual void move(const sf::Vector2f& delta) override;
+
+	virtual void onUpdate() override;
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+private:
+	float _timer;
+	sf::Sprite _sprite;
+};
